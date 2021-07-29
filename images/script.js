1091b9b32656b1bff410442b5f3d7c813f2fd64a
tannerlsmith <tanner.smith@uvm.edu>
2021-07-29 10:49:39 -0400

    styles, setup

--- images/script.js
+++
@@ -1,8 +0,0 @@
-
-object.addEventListener('btn', checkAnswers())
-
-function checkAnswers() {
-    var str = 'Hello World'
-    var result = str.fontcolor('red')
-    document.getElementById('tf-display').innerHTML = result
-}
